35e124a0ec815ae59a00e5745808f766987f49f2
Panny Lewis <pannylewis@gmail.com>
2020-08-11 13:10:39 -0500

    MySecretFriends.js:  using class component, set up states, trying to render a new page header, importing FriendsCards component. FriendsCards.js: will receive props from parent component once axios call completed.

---
+++ friends/src/components/FriendsCards.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const FriendsCards = (props) => {
+  return <div>hey there minions</div>;
+};
+export default FriendsCards;
--- friends/src/components/MySecretFriends.js
+++ friends/src/components/MySecretFriends.js
@@ -1,7 +1,39 @@
 import React from "react";
+import axios from "axios";
+import FriendsCards from "./FriendsCards";
 
-const MySecretFriends = () => {
-  return <div>All my little minions!</div>;
-};
+// const MySecretFriends = () => {
+//   return <div>All my little minions!</div>;
+// };
+
+// export default MySecretFriends;
+
+class MySecretFriends extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "", age: "", email: "" };
+  }
+
+  componentDidMount() {
+    axios.get().then().catch();
+  }
+
+  render() {
+    return (
+      <div className="friendsPage">
+        <div className="header">
+          <h1>Welcome Friends</h1>
+          <ol>
+            <li>Friends</li>
+            <li>Add Friends</li>
+            <li>Log out</li>
+          </ol>
+        </div>
+
+        <FriendsCards />
+      </div>
+    );
+  }
+}
 
 export default MySecretFriends;
